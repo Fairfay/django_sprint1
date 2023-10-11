8e2800bac979da23fc78ffe3f90b2c55c9169261
unknown <dencat87@gmail.com>
2023-10-11 18:06:51 +0700

    Fix pep8

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def about(request):
     template = 'pages/about.html'
     return render(request, template)
-    
+
 
 def rules(request):
     template = 'pages/rules.html'
